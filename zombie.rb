fd77932584d1f9ed794a05aa87e19354fc8cb910
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-15 23:50:55 -0400

    added initialize,all, and spawn states

--- zombie.rb
+++ zombie.rb
@@ -1,12 +1,13 @@
 class Zombie
 @@horde = [] # contain the collection of all zombies
-@@plague_level = 0 # will increase over time
+@@plague_level = 10 # will increase over time
 @@max_speed = 5 # speed attribute of zombies, will not change
 @max_strength = 8 # max_strength attribute of any zombie
 @@default_speed = 1 # value will not change
 @@default_strength = 3 # value will not change
 
   def self.all
+    @@horde
   end
 
   def self.new_day
@@ -15,13 +16,26 @@ class Zombie
   def self.some_die_off
   end
 
-  def self.self.spawn
+  def self.spawn
+    rand(@@plague_level).times do
+      speed = rand(@@max_speed)
+      strength = rand(@@max_strength)
+      @@horde << Zombie.new(speed,strength)
+    end
   end
 
   def self.increase_plague_level
   end
 
-  def initialize
+  def initialize(speed,strength)
+    if speed < @@max_speed
+      @@default_speed
+    else @zombie_speed = speed
+    end
+    if strength < @@max_strength
+      @@default_strength
+    else @zombie_strength = strength
+    end
   end
 
   def encounter
