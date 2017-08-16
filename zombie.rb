ee0cc84a14a4566910edf1cebaf114af0c0aa7c3
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-15 22:49:19 -0400

    setup game, added variables

---
+++ zombie.rb
@@ -0,0 +1,36 @@
+class Zombie
+@@horde = [] # contain the collection of all zombies
+@@plague_level = 0 # will increase over time
+@@max_speed = 5 # speed attribute of zombies, will not change
+@max_strength = 8 # max_strength attribute of any zombie
+@@default_speed = 1 # value will not change
+@@default_strength = 3 # value will not change
+
+  def self.all
+  end
+
+  def self.new_day
+  end
+
+  def self.some_die_off
+  end
+
+  def self.self.spawn
+  end
+
+  def self.increase_plague_level
+  end
+
+  def initialize
+  end
+
+  def encounter
+  end
+
+  def outrun_zombie?
+  end
+
+  def survive_attack?
+  end
+
+end
